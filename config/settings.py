058da0a7af078a10eed693e656df0c91e5827b22
elbek2327 <zubaydullayev1609@gmail.com>
2025-05-14 00:12:51 +0500

    dotenv removed

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("DJANGO_SECURITY_KEY")
+SECRET_KEY = 'django-insecure--**%8r2l67lum@oz+m%!^h*-(&)hs0m4=7w0j4+d6bv2h-2kqh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
